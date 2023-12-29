07135e4532946c64f86a0a2062e3d787ba69a4ba
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-29 12:19:07 +0100

    + message from home

---
+++ 220511_message.py
@@ -0,0 +1,27 @@
+line = open(0).read().strip() # .py < _infile_
+binstr = '000'+bin(int(line, 16))[2:]
+
+# get the table
+D = {}
+with open('infile') as file:
+    for line in file:
+        val, key = line.strip().split(' ')
+        D[key] = val
+
+res=''
+l = 0
+while l < len(binstr) - 3:
+    r = l + 4
+    char = binstr[l:r]
+    if char not in D:
+        r += 1
+        char = binstr[l:r]
+    if char not in D:
+        r += 2
+        char = binstr[l:r]
+    if char not in D:
+        break
+    res += D[char]
+    l = r
+print(res)
+

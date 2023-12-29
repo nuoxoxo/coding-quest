45f07b60cc44b2bb7dea6dc1db7a81ef65f02fc4
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-29 13:51:54 +0100

    + 1 engine : done

--- 01_engine.py
+++ 01_engine.py
@@ -2,7 +2,8 @@ lines = list(map(int, open(0).read().splitlines()))
 
 res,cur,end=0,0,60
 while end < len(lines) + 1:
-    res += 1 if not (1500 <= sum(lines[cur:end]) // 60 <= 1600) else 0
+    res += not 1500 <= sum(lines[cur:end]) // 60 <= 1600
     cur += 1
     end += 1
 print(res)
+assert(res in [6248])

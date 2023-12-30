2dd6e31ae62722d396e2f032b8abfd3adbd43cf0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-30 14:27:57 +0100

    + problem 6 : done . cleanup, passed all tests

--- 06_dbg.py
+++ 06_dbg.py
@@ -1,67 +1,48 @@
 lines = open(0).read().splitlines()
 
 from collections import defaultdict
+
 D = defaultdict(int)
 cmp = False
+res = []
 
-res = None
+def rvalue(R) :
+    return int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
+
 i = 0
-#for i, line in enumerate(lines):
+
 while True:
-    print('line:',i)
-    line = lines[i]
-    expr = line.split(' ')
-    # print(expr[0], expr)
+    #print('Nr/',i)
+    expr = lines[i].split(' ')
     CMD = expr[0]
     assert len (expr) in [1,2,3]
-    if len(expr) == 1: break
+    if len(expr) == 1: # END
+        break
     elif len(expr) == 2:
-        R = expr[1]
+        R = rvalue( expr[1] )
         if CMD == 'OUT':
-            R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
-            print(R)
-            res = R
-        elif CMD == 'JIF' and cmp:
-            R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
-            #print(i, R, i+R, len(lines))
-            i = (i+R) % len(lines)
-            continue
-        elif CMD == 'JMP':
-            R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
-            #print(i, R, i+R, len(lines))
+            print('out/', R)
+            res.append(R)
+        elif CMD == 'JIF' and cmp or CMD == 'JMP':
             i = (i+R) % len(lines)
             continue
     elif len(expr) == 3:
-        # print(expr)
         L, R = expr[1:]
+        R = rvalue(R)
         assert isinstance(L, str)
         match CMD:
-            case 'ADD':
-                R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
-                # R = int(R) if R.isdigit() or R[1:].isdigit() else D[R]
-                D[L] += R
+            case 'MOV': D[L] = R
+            case 'ADD': D[L] += R
+            case 'CEQ': cmp = D[L] == R
+            case 'CGE': cmp = D[L] >= R
             case DM if DM in ['MOD','DIV']:
-                R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
-                # R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
                 assert R != 0
                 if DM == 'MOD':
                     D[L] %= R
                 else:
                     D[L] //= R
-            case 'MOV':
-                R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
-                # R = int(R) if R.lstrip('-1').isdigit() else D[R]
-                D[L] = R
-            case 'CEQ':
-                R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
-                assert i < len(lines) and lines[i+1].startswith('JIF')
-                # R = int(R) if R.lstrip('-1').isdigit() else D[R]
-                cmp = D[L] == R
-            case 'CGE':
-                assert i < len(lines) and lines[i+1].startswith('JIF')
-                R = int(R) if R.isdigit() or len(R)>1 and R[1:].isdigit() else D[R]
-                # R = int(R) if R.lstrip('-1').isdigit() else D[R]
-                cmp = D[L] >= R
     i += 1
 
-assert res in [ 7745743850156157 ]
+assert len(res) in [1, 10]
+assert res[0] in [ 1, 5, 111, 7745743850156157 ]
+

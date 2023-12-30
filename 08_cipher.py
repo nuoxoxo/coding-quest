e260ec27eafe7670d07fe308d36eba202147435a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-31 00:34:42 +0100

    + pb 8 : bug in decode beginning keyindex count

---
+++ 08_cipher.py
@@ -0,0 +1,95 @@
+todecode = open(0).read().strip()
+charset = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,;:?! '()"""
+key = """Roads? Where We're Going, We Don't Need Roads."""
+print('encoded/',todecode)
+print('charset/',charset)
+print('key/',key,'\n')
+
+"""
+offset = key.indexof(char)
+'A' -> ( charset.indexof('A') + offset ) % len(charset)
+"""
+
+def DEC(key, charset, emsg):
+    # know ending position in key
+    count = -1
+    for c in emsg:
+        count += 1
+    keyindex = count % len(key)
+    print('keyindex/', keyindex)
+    msgindex = len(emsg) - 1
+    res = ''
+    while msgindex > -1:
+        msgchar = emsg[ msgindex ]
+        if msgchar not in charset:
+            res += msgchar
+            msgindex -= 1
+            keyindex = (keyindex - 1)%len(key)# + len(key)) % len(key)
+            continue
+        # get offset
+        keychar = key[ keyindex ]
+        keycharoffset = charset.index(keychar) + 1# % len(charset)
+        # move by offset then get the char
+        msgcharindex = charset.index(msgchar)
+        nextcharindex = (msgcharindex - keycharoffset) % len(charset)# + len(charset)) % len(charset)
+        nextchar = charset[ nextcharindex ]
+        res += nextchar
+        msgindex -= 1
+        keyindex = (keyindex - 1)%len(key)# + len(key)) % len(key)
+    print('res1/',res)
+    res = ''.join(reversed(res))
+    print('res2/',res)
+    return res
+
+res=DEC(key, charset, todecode)
+
+K='With great power comes great responsibility'
+CS='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,;:?! \'()'
+EM='lfwwrsvbvMbmIEnK:wDjutpzoxfwowypDDHxB(rzfwKXBMd'
+should_be='I could use this to pass secret notes in class!'
+res=DEC(K, CS, EM)
+assert res == should_be
+
+def ENC(key, charset, msg):
+    keyindex = 0
+    msgindex = 0
+    res = ''
+    while msgindex < len(msg):
+        msgchar = msg[ msgindex ]
+        if msgchar not in charset:
+            res += msgchar
+            msgindex += 1
+            keyindex = (keyindex+1) % len(key)
+            continue
+        # get offset
+        keychar = key[ keyindex ]
+        keycharoffset = (charset.index(keychar) + 1) % len(charset)
+        # move by offset then get the char
+        msgcharindex = charset.index(msgchar)
+        nextcharindex = (msgcharindex + keycharoffset) % len(charset)
+        res += charset[ nextcharindex ]
+        # wrap around curr index in key
+        keyindex = (keyindex+1) % len(key)
+        msgindex += 1
+    print('res/',res)
+    return res
+K='SECRET'
+CS='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+M='WE COME IN PEACE'
+res=ENC(K, CS, M)
+assert res == 'PJ UTGX LF JXFFW'
+
+K='With great power comes great responsibility'
+CS='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,;:?! \'()'
+M='Are you enjoying coding quest?'
+res=ENC(K, CS, M)
+assert res == 'dAyevvMbfHgENFsy:fDqnGddIzfMqm'
+
+K='With great power comes great responsibility'
+CS='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,;:?! \'()'
+M='I could use this to pass secret notes in class!'
+res=ENC(K, CS, M)
+assert res == 'lfwwrsvbvMbmIEnK:wDjutpzoxfwowypDDHxB(rzfwKXBMd'
+
+
+

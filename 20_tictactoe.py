24cabcef6fa948152bedf0333e8c0e8d5830e802
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-31 15:58:58 +0100

    + pb 20 : done - Tic-tac-toe brute-forced

--- 20_tictactoe.py
+++ 20_tictactoe.py
@@ -1,43 +1,46 @@
+# tic-tac-toe bruteforced
+
 def play(T, p):
     t = list(dict(sorted(T.items())).values())
+    # 3 rows
     t = [ t[0:3], t[3:6], t[6:9] ]
-    if (any(n == 0 for n in t)):
-        return ( False, 'Draw' )
-    if (all(n > 0 and n == p for n in t[0])):
+    if all(n == p for n in t[0]) \
+    or all(n == p for n in t[1]) \
+    or all(n == p for n in t[2]):
         return ( True, p )
-    if (all(n > 0 and n == p for n in t[1])):
+    # 3 cols
+    if all(n == p for n in list(zip(*t))[0]) \
+    or all(n == p for n in list(zip(*t))[1]) \
+    or all(n == p for n in list(zip(*t))[2]):
         return ( True, p )
-    if (all(n > 0 and n == p for n in t[2])):
-        return ( True, p )
-    if (all(n > 0 and n == p for n in list(zip(*t))[0])):
-        return ( True, p )
-    if (all(n > 0 and n == p for n in list(zip(*t))[1])):
-        return ( True, p )
-    if (all(n > 0 and n == p for n in list(zip(*t))[2])):
-        return ( True, p )
+    # diagonal x2
     t = [n for row in t for n in row]
-    if (all(n > 0 and n == p for n in [ t[0],t[4],t[8] ])):
+    if all(n == p for n in [ t[0],t[4],t[8] ]) \
+    or all(n == p for n in [ t[2],t[4],t[6] ]):
         return ( True, p )
-    if (all(n > 0 and n == p for n in [ t[2],t[4],t[6] ])):
-        return ( True, p )
-    return ( False, 'Unfinished' )
+    # draw|unfinished if all win-conds skipped
+    return ( True, 'Draw' ) if all(n != 0 for n in t) else ( False, 'Unfinished' )
 
-res = [0, 0, 0] 
+end = [0, 0, 0] 
 for line in [[int(_) for _ in line.split()] for line in open(0).read().splitlines()]:
-    print(line)
     T = {}
     for i in range(1,10): T[i] = 0
     for i, n in enumerate(line):
         p = i % 2 + 1
         T[n] = p
-        win, player = play(T, p)
-        if win:
-            res[i % 2] += 1
+        endgame, verdict = play(T, p)
+        if endgame:
+            if verdict == 'Draw':
+                end[2] += 1
+            else:
+                end[ verdict-1 ] += 1
             break
-        elif player == 'Draw':
-            res[2] += 1
-end = 1
-for n in res:
-    end *= n
-print(end)
+
+res = 1
+for n in end:
+    res *= n
+print('res/', res)
+print('end/', end)
+
+assert res in [ 20938290, 0 ]
 

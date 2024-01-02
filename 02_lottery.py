8dd0f78d8a35873a581e639c4e330634036800d4
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-02 20:09:20 +0100

    upd. 02 lottery : no sum(arr)

--- 02_lottery.py
+++ 02_lottery.py
@@ -2,7 +2,7 @@ lines = [list(map(int, line.split())) for line in open (0).read().splitlines()]
 D = {3:1,4:10,5:100,6:1000}
 W = list(map(int, '12 48 30 95 15 55 97'.split(' ')))
 
-T = []
+res = 0
 for line in lines:
     wins = 0
     for n in line:
@@ -10,7 +10,7 @@ for line in lines:
             wins += 1
     if wins not in D:
         assert (wins < max(D.keys()))
-    if wins in D:
-        T.append(D[wins])
-print(sum(T))
-assert sum(T) in [ 56 ]
+    res += D[wins] if wins in D else 0
+
+print('res/',res)
+assert res in [110, 56]

4b072b75039c5e058ad5c757f752f900a48e56ba
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-04 17:00:54 +0100

    combine 2 ways of pb 15

--- 15_asteroid.py
+++ 15_asteroid.py
@@ -1,25 +1,51 @@
 G = [ [int(d) for d in _.split()] for _ in open(0).read().splitlines()]
 
-dr=[-1,0,1, 0]
-dc=[ 0,1,0,-1]
-R, C = len(G), len(G[0])
-seen=set()
-res = 0
-N = 0
+def way1(G) -> int:
+    dr=[-1,0,1, 0]
+    dc=[ 0,1,0,-1]
+    R, C = len(G), len(G[0])
+    seen=set()
+    res = 0
+    N = 0
+    def DFS(r,c,current) -> int:
+        if -1 < r < R and -1 < c < C and G[r][c] != 0 and (r,c) not in seen:
+            seen.add( (r,c) )
+            current += G[r][c]
+            for i in range(4):
+                current = DFS(r + dr[i], c + dc[i], current)
+        return current
+    for r in range(R):
+        for c in range(C):
+            if G[r][c] != 0 and (r,c) not in seen:
+                res += DFS(r, c, 0)
+                N += 1
+    return res//N
 
-def DFS(r,c,current) -> int:
-    if -1 < r < R and -1 < c < C and G[r][c] != 0 and (r,c) not in seen:
-        seen.add( (r,c) )
-        current += G[r][c]
-        for i in range(4):
-            current = DFS(r + dr[i], c + dc[i], current)
-    return current
+def way2(G) -> int:
+    dr=[-1,0,1, 0]
+    dc=[ 0,1,0,-1]
+    R, C = len(G), len(G[0])
+    seen=set()
+    res = 0
+    N = 0
+    def DFS_2(r,c) -> int:
+        if -1 < r < R and -1 < c < C and G[r][c] != 0 and (r,c) not in seen:
+            seen.add( (r,c) )
+            curr = G[r][c]
+            for i in range(4):
+                curr += DFS_2( r + dr[i], c + dc[i] )
+            return curr
+        return 0
+    for r in range(R):
+        for c in range(C):
+            if G[r][c] != 0 and (r,c) not in seen:
+                res += DFS_2(r, c)
+                N += 1
+    return res//N
 
-for r in range(R):
-    for c in range(C):
-        if G[r][c] != 0 and (r,c) not in seen:
-            res += DFS(r, c, 0)
-            N += 1
+res = way1(G)
+res2 = way2(G)
 
-print(res//N)
-assert res//N in [33]
+assert res == res2
+assert res in [ 33 ]
+print(res)

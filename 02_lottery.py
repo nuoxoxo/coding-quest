90327c7806d333c495ba1abde45c03f53f869849
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-28 20:44:25 +0100

    1/py : edit - using regex instead of list comprehension

--- 02_lottery.py
+++ 02_lottery.py
@@ -1,4 +1,14 @@
-lines = [list(map(int, line.split())) for line in open (0).read().splitlines()]
+# Get input : Way 1 - list comprehension
+#lines = [list(map(int, line.split())) for line in open (0).read().splitlines()]
+
+# Get input : Way 2 - regex
+
+import re
+lines = open(0).read().splitlines()
+for i in range(len(lines)):
+    lines[i] = list(map(int, re.findall(r"\d+", lines[i])))
+
+print(lines)
 D = {3:1,4:10,5:100,6:1000}
 W = list(map(int, '12 48 30 95 15 55 97'.split(' ')))
 

06a1cd28fff96acbbb498c68f1c851b66a9f060f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-16 11:11:45 +0100

    36/added assert

--- 36_shortest_path.py
+++ 36_shortest_path.py
@@ -33,17 +33,15 @@ def Solver(G):
                 Q.append(((1 - L, rr, cc), res + 1))
     return -1
 
-G = [_.splitlines() for _ in open('36.1').read().split('\n\n')]
-for g in G:
-    for gg in g:print(gg)
-    print()
-res = Solver(G)
-print('/test', res)
-# assert res == 53
+RES = []
 
-G = [_.splitlines() for _ in open('36.0').read().split('\n\n')]
-res = Solver(G)
-print('/real', res)
-#assert res == 250
+for i in [0,1]:
+    # G = [_.splitlines() for _ in open('36.1').read().split('\n\n')]
+    G = [[[c for c in s] for s in _.splitlines()] \
+        for _ in open('36.' + str(i)).read().split('\n\n')]
+    res = Solver(G)
+    RES.append(res)
 
+assert all([_ in [251, 53] for _ in RES])
 # attempts 258 257 241 251 245
+
